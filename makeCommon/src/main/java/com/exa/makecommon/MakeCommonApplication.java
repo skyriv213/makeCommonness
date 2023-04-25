d56483e6432eaff0b04852ded0f0e5e5fdcc2374
skyriv213 <gn1007@dankook.ac.kr>
2023-04-25 22:18:04 +0900

    기본 프로젝트 생성

---
+++ makeCommon/src/main/java/com/exa/makecommon/MakeCommonApplication.java
@@ -0,0 +1,13 @@
+package com.exa.makecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeCommonApplication.class, args);
+    }
+
+}
---
+++ makeCommon/src/test/java/com/exa/makecommon/MakeCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exa.makecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
